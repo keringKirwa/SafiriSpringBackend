40d72b3381c9084a19d9eb8c184312f49b39dc7f
Kirwa <kkirwa230@gmail.com>
2022-07-02 00:16:50 +0300

    first commit

---
+++ src/main/java/com/kirwa/safiriApp/SafiriAppApplication.java
@@ -0,0 +1,14 @@
+package com.kirwa.safiriApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafiriAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SafiriAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirwa/safiriApp/SafiriAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirwa.safiriApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafiriAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
